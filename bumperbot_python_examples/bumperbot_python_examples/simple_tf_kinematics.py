84446a4f38dc1705016569e489fc1d4ac5a31059
yacin <yacin.ha9@gmail.com>
2025-06-11 15:12:33 +0100

    add a service server that gave the transform between two frame

--- bumperbot_python_examples/bumperbot_python_examples/simple_tf_kinematics.py
+++ bumperbot_python_examples/bumperbot_python_examples/simple_tf_kinematics.py
@@ -1,8 +1,11 @@
 import rclpy
 from rclpy.node import Node
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-from tf2_ros import TransformBroadcaster
+from tf2_ros import TransformBroadcaster, TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import TransformStamped
+from bumperbot_msgs.srv import GetTransform
 import math
 
 
@@ -15,6 +18,9 @@ class SimpleTFKinematics(Node):
 
         self.static_transform_stamped = TransformStamped()
         self.dynamic_transform_stamped = TransformStamped()
+
+        self.tf_buffer_ = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer_, self)
 
         self.x_increment_ = 0.01
         self.last_x_= 0.0
@@ -38,6 +44,8 @@ class SimpleTFKinematics(Node):
         
         self.timer_ = self.create_timer(0.1, self.timerCallback)
 
+        self.server_ = self.create_service(GetTransform, "get_transform", self.transformCallback)
+
 
     def timerCallback(self):    
         self.dynamic_transform_stamped.header.stamp = self.get_clock().now().to_msg()
@@ -54,6 +62,20 @@ class SimpleTFKinematics(Node):
 
         self.last_x_ = self.dynamic_transform_stamped.transform.rotation.x
         self.dynamic_tf_broadcaster.sendTransform(self.dynamic_transform_stamped)
+
+    def transformCallback(self, req, res):
+        self.get_logger().info(f"Requested transform between {req.frame_id} and {req.child_frame_id}")
+        requested_transform = TransformStamped()
+        try:
+            requested_transform = self.tf_buffer_.lookup_transform(req.frame_id, req.child_frame_id, rclpy.time.Time())
+        except TransformException as e :
+            res.success = False
+            return res
+        
+        res.success = True
+        res.transform = requested_transform
+        return res
+
 
 def main():
     rclpy.init()
