0217c62d88a8f92e4a6df0ea1dac2df528f78abc
yacin <yacin.ha9@gmail.com>
2025-07-14 22:12:56 +0100

    add decrease speed functionality

--- bumperbot_utils/bumperbot_utils/safety_stop.py
+++ bumperbot_utils/bumperbot_utils/safety_stop.py
@@ -2,6 +2,8 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
+from twist_mux_msgs.action import JoyTurbo
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 from enum import Enum
@@ -17,10 +19,12 @@ class SafetyStop(Node):
         super().__init__("safety_stop")
 
         self.declare_parameter("danger_distance", 0.2)
+        self.declare_parameter("warning_distance", 0.6)
         self.declare_parameter("scan_topic", "scan")
         self.declare_parameter("safety_stop_topic", "safety_stop")
         
         self.danger_distance = self.get_parameter("danger_distance").get_parameter_value().double_value
+        self.warning_distance = self.get_parameter("warning_distance").get_parameter_value().double_value
         self.scan_topic = self.get_parameter("scan_topic").get_parameter_value().string_value
         self.safety_stop_topic = self.get_parameter("safety_stop_topic").get_parameter_value().string_value
 
@@ -32,23 +36,47 @@ class SafetyStop(Node):
                                                      self.safety_stop_topic, 
                                                      10)
         
+        self.decrease_speed_client = ActionClient(self, 
+                                                  JoyTurbo, 
+                                                  "joy_turbo_decrease")
+        self.increase_speed_client = ActionClient(self, 
+                                                  JoyTurbo, 
+                                                  "joy_turbo_increase")
+        
+        while not self.decrease_speed_client.wait_for_server(timeout_sec=1.0) and rclpy.ok():
+            self.get_logger().warn("/joy_turbo_decrease is not available waiting...")
+        
+        while not self.increase_speed_client.wait_for_server(timeout_sec=1.0) and rclpy.ok():
+            self.get_logger().warn("/joy_turbo_increase is not available waiting...")
+        
+        
+
+        
         self.state = State.FREE
+        self.prev_state = State.FREE
 
     def laserCallback(self, msg: LaserScan):
         self.state = State.FREE
 
         for range_value in msg.ranges:
-            if not math.isinf(range_value) and range_value <= self.danger_distance:
-                self.state = State.DANGER
-                break
+            if not math.isinf(range_value) and range_value <= self.warning_distance:
+                self.state = State.WARNING
+                if range_value <= self.warning_distance:
+                    self.state = State.DANGER
+                    break
 
-        is_safety_stop = Bool()
-        if self.state == State.FREE:
-            is_safety_stop.data = False
-        elif self.state == State.DANGER:
-            is_safety_stop.data = True
-
-        self.safety_stop_pub.publish(is_safety_stop)
+        if self.state != self.prev_state:
+            is_safety_stop = Bool()
+            if self.state == State.WARNING: 
+                is_safety_stop.data = False
+                self.decrease_speed_client.send_goal_async(JoyTurbo.Goal())
+            if self.state == State.FREE:
+                is_safety_stop.data = False
+                self.increase_speed_client.send_goal_async(JoyTurbo.Goal())
+            elif self.state == State.DANGER:
+                is_safety_stop.data = True
+            self.prev_state = self.state
+            self.safety_stop_pub.publish(is_safety_stop)
 
 
 def main():
